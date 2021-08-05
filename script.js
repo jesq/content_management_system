8b36755756f0df216c96de655a45d231be51a553
Cosmin Jescu <57216949+jesq@users.noreply.github.com>
2021-08-05 18:37:34 +0300

    Added a function to insert the data in the table

--- script.js
+++ script.js
@@ -1,8 +1,30 @@
 function onFormSubmit(){
-
+    var formData = readFormData();
+    insertNewRecord(formData);
 }
 
 function readFormData(){
     var formData = {};
     formData["firstName"] = document.getElementById("firstName").value;
+    formData["lastName"] = document.getElementById("lastName").value;
+    formData["emailAddress"] = document.getElementById("emailAddress").value;
+    return formData;
+}
+
+function insertNewRecord(data) {
+    var table = document.getElementById("employeeTable").getElementsByTagName('tbody')[0];
+    var newRow = table.insertRow(table.length);
+
+    cell1 = newRow.insertCell(0);
+    cell1.innerHTML = data.firstName;
+
+    cell2 = newRow.insertCell(1);
+    cell2.innerHTML = data.lastName;
+
+    cell3 = newRow.insertCell(2);
+    cell3.innerHTML = data.emailAddress;
+
+    cell4 = newRow.insertCell(3);
+    cell4.innerHTML = `<a>Edit</a>
+                       <a>Delete</a>`;
 }
